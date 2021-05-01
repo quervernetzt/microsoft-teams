53073144ab88e31d349a40676e75ec0fe5b7fad9
quervernetzt <info@quervernetzt.de>
2021-05-01 08:01:44 +0200

    starting with work on private channel import

---
+++ add-members-private-channel/README.md
@@ -0,0 +1,17 @@
+# General
+
+- Goal is to import members to a Private Channel and assign tags
+
+
+# How to use it
+
+- Execute the script providing the required parameters
+
+- Example
+
+
+# Resources
+
+[1] [Microsoft Teams PowerShell Overview](https://docs.microsoft.com/en-us/microsoftteams/teams-powershell-overview)
+
+[2] [MicrosoftTeamsPowerShell](https://docs.microsoft.com/en-us/powershell/module/teams/?view=teams-ps)
---
+++ add-members-private-channel/script.ps1
@@ -0,0 +1,73 @@
+# Parameters
+[CmdletBinding()]
+param (
+    [Parameter(
+        Mandatory = $false,
+        HelpMessage = "Please enter the path to the CSV with members email addresses to add",
+        Position = 1)
+    ]
+    [string]
+    $MembersToImportCSVPath = "$PSScriptRoot\members.csv"
+
+    # [Parameter(
+    #     Mandatory = $true,
+    #     HelpMessage = "Please enter the name of the Teams group",
+    #     Position = 2)
+    # ]
+    # [string]
+    # $TeamsGroupName,
+
+    # [Parameter(
+    #     Mandatory = $true,
+    #     HelpMessage = "Please enter the name of the Private Channel to add the members to",
+    #     Position = 3)
+    # ]
+    # [string]
+    # $TeamsGroupPrivateChannelName,
+
+    # [Parameter(
+    #     Mandatory = $false,
+    #     HelpMessage = "Please enter the tags as comma separated string (e.g. tag1,tag2) that shall be assigned to the members",
+    #     Position = 4)
+    # ]
+    # [string]
+    # $TagsToAssign = ""
+)
+
+# Install PS Teams module
+# $moduleName = "MicrosoftTeams"
+# if (Get-Module -Name $moduleName -ListAvailable) {
+#     $toUpdate = Read-Host "Module '$moduleName' is available, do you want to update it? (yes/no)"
+#     if ($toUpdate.Trim().ToLower() -eq "yes") {
+#         Update-Module $moduleName -Force
+#     }
+# }
+# else {
+#     Write-Host "Installing module '$moduleName'..."
+#     Install-Module $moduleName -Scope CurrentUser -Force
+# }
+
+# Write-Host "Checked module '$moduleName'..."
+
+
+# # Login
+# Connect-MicrosoftTeams
+
+# Get email addresses from CSV
+[pscustomobject]$members = Import-Csv -Delimiter "," -Path $MembersToImportCSVPath
+
+foreach ($member in $members) {
+    [string]$emailAddress = $member.Email
+    Write-Host "Processing member with email address '$emailAddress'..."
+}
+
+# Add members to Teams Group (if not already in Teams Group)
+
+
+# Add members to Private Channel (if not already in the channel)
+
+
+# Assign the tags
+
+
+Write-Host "Done..."
